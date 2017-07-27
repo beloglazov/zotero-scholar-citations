a03c7367d59801a90d3784d3ec02a7f80e65b652
Anton Beloglazov <anton.beloglazov@gmail.com>
2017-07-26 20:57:12 -0700

    Add a 1.9.2 build with support for Zotero 5

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-version=1.9.1
+version=1.9.2
 rm builds/zotero-scholar-citations-${version}-fx.xpi
 zip -r builds/zotero-scholar-citations-${version}-fx.xpi chrome/* chrome.manifest install.rdf
